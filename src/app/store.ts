bcfbcf6c6d320c188d10750d7b7abb52422a98e4
jiwonjung <4dakto@gmail.com>
2021-06-22 18:49:39 +0900

    add basic setup for redux

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import counterReducer from "../features/counter/counter.slice";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
